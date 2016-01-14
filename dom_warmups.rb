0bc32def2faa146a027013448a69b11a1bdb1daa
Andrew Baik <and.baik@gmail.com>
2016-01-14 14:56:47 -0500

    setup DomParser class

--- dom_warmups.rb
+++ dom_warmups.rb
@@ -3,14 +3,22 @@ Tag = Struct.new(:text, :type, :classes, :id, :name, :children, :parent)
 class DomParser
 
   def initialize
+    @html_string = File.open("test.html").readlines[1..-1].map(&:strip).join
     @root = nil # html?
     @depth = 0
+
   end
 
-  #returns Hash or Struct after parsing HTML
-  def parse_tag(input)
+  def set_root_node
+    @root = parse_tag()
+  end
 
+  #returns the top level tag
+  #and update the @html_string
+  def parse_tag
     tag = Tag.new
+    opening_tag = @html_string.match(/^<.*?>/).to_s
+
 
     tag_type_regex = /<[a-z]*[0-9]*/
     class_regex = /class=('|")[[a-z0-9]*\W*\s]*?('|")/
@@ -18,39 +26,31 @@ class DomParser
     name_regex = /name=('|")[[a-z0-9]*\W*]*?('|")/
     # text_regex = />(\s*.*?\s*)</
 
-    tag.type = input.match(tag_type_regex).to_s[1..-1]
-    tag.classes = input.match(class_regex).to_s[7..-2].split(' ')
-    tag.id = input.match(id_regex).to_s[4..-2]
-    tag.name = input.match(name_regex).to_s[6..-2]
-    return tag
+    tag.type = opening_tag.match(tag_type_regex).to_s[1..-1]
+    tag.classes = opening_tag.match(class_regex).to_s[7..-2].split(' ') if opening_tag.match(class_regex)
+    tag.id = opening_tag.match(id_regex).to_s[4..-2]
+    tag.name = opening_tag.match(name_regex).to_s[6..-2]
+    p tag
   end
-
-  tag = parse_tag("<h3 id='baz123' class='foo-bar foo bar test-ing' name='fozzie'>bla bla bla</h3>")
-
-  p tag.type
-  p tag.classes
-  p tag.id
-  p tag.name
-
-
-  # <div>
-  #   div text before
-  #   <p>
-  #     p text
-  #   </p>
-  #   <div>
-  #     more div text
-  #   </div>
-  #   div text after
-  # </div>
 
   # make our tree
   def parser_script
+    #create a outermost Tag (with top level tag)
 
-
+    #cut the html string without extracted tag
+    #recurse the above two steps until cut html string doesn't have a tag anymore
 
   end
 
-
 end
 
+# tag = parse_tag("<h3 id='baz123' class='foo-bar foo bar test-ing' name='fozzie'>bla bla bla</h3>")
+#
+# p tag.type
+# p tag.classes
+# p tag.id
+# p tag.name
+
+#p file = File.open("test.html").readlines[1..-1].map(&:strip).join
+game = DomParser.new
+game.parse_tag
---
+++ pseudocode.rb
@@ -0,0 +1,5 @@
+#html string extracted
+
+  #create a outermost Tag (with top level tag)
+  #cut the html string without extracted tag
+  #recurse the above two steps until cut html string doesn't have a tag anymore
